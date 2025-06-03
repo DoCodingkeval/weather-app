f5a381f48bc6820e4a83be0569bb9399426fdad4
DoCodingkeval <gohelkeval7@gmail.com>
2025-06-03 13:08:54 +0530

    commit

--- src/components/Noresult.jsx
+++ src/components/Noresult.jsx
@@ -7,8 +7,8 @@ const Noresult = () => {
     <div className="flex items-center justify-center flex-col text-center p-2.5 font-bold"
     style={{background: bgColor}}>
       <img className="w-35" src={nothing} alt="noresult-image" />
-      <span className="m-4 text-2xl text-white">Something went wrong</span>
-      <p className="text-white px-10 text-[#333] text-[16px]">We&apos;re unable to retrieve the weather details. Ensure you&apos;ve entered a valid city or try again later.</p>
+      <span className="m-2 text-2xl text-white">Something went wrong</span>
+      <p className="text-white px-2 text-[16px]">We&apos;re unable to retrieve the weather details. Ensure you&apos;ve entered a valid city or try again later.</p>
     </div>
   )
 }
